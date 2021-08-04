6815fe80a770a940fc88138569f96798763333a6
Samuel <samuelgsete@gmail.com>
2021-08-04 11:59:15 -0300

    Correção no algoritimo Substitution

--- Linguagem C/Substitution/substitution.c
+++ Linguagem C/Substitution/substitution.c
@@ -5,30 +5,55 @@
 #include <string.h>
 
 bool validarChave(string chave) {
-    int n = strlen(chave);
-    /* Verifica se a chave possui exatamente 26 caracteres */
-    if(n != 26) {
+
+    /* Verifica se a chave possui 26 caracteres */
+    if(strlen(chave) != 26) {
         return false;
     }
 
     /* Verifica se a chave contém somente caracteres alfabéticos */
-    for(int i = 0; i < n; i++) {
+    int i = 0;
+    while(chave[i] != '\0') {
         if(!isalpha(chave[i])) {
             return false;
         }
+        i++;
     }
 
     /* Verifica se não há letras repetidas */
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(chave[i] == chave[j]) {
+    i = 0;
+    int j;
+    for (i = 0; i < 26; i++) {
+        for (j = i + 1; j < 26; j++) {
+            if (chave[i] == chave[j]) {
                 return false;
             }
         }
     }
 
     return true;
+}
 
+char substituir(char c, string chave) {
+
+    string alfabetoMaiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string alfabetoMinusculo = "abcdefghijklmnopqrstuvwxyz";
+    int c_i = '\0';
+
+    printf("Testando: %c \n", c);
+
+    for(int i=0; i < 26; i++) {
+        if(c == alfabetoMaiusculo[i]) {
+            c_i = chave[i];
+            break;
+        }
+        if(c == alfabetoMinusculo[i]) {
+            c_i = chave[i] + 32; // convertendo para minúsculo
+            break;
+        }
+
+    }
+    return c_i;
 }
 
 string converterParaMaiuscula(string chave) {
@@ -37,26 +62,6 @@ string converterParaMaiuscula(string chave) {
         chave[i] = (chave[i] >= 'a' && chave[i] <= 'z') ? chave[i] - 32 : chave[i];
     }
     return chave;
-}
-
-char substituir(char c, string chave) {
-    string alfabetoMaiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string alfabetoMinusculo = "abcdefghijklmnopqrstuvwxyz";
-    char c_i = '\0';
-
-    for(int i = 0; i < 26; i++) {
-        if(c == alfabetoMaiusculo[i]) {
-            c_i = chave[i];
-            break;
-        }
-
-        if(c == alfabetoMinusculo[i]) {
-            c_i = chave[i] + 32; // convertendo para minúsculo
-            break;
-        }
-    }
-
-    return c_i;
 }
 
 int main(int argc, char *argv[]) {
@@ -68,35 +73,33 @@ int main(int argc, char *argv[]) {
 
     string chave = argv[1];
     chave = converterParaMaiuscula(chave);
-
     printf("Key: ");
     fputs(chave, stdout);
     printf("\n");
 
     if(!validarChave(chave)) {
-        printf("Chave inválida\n");
+        printf("Chave inválida!\n");
         return 1;
     }
 
     string p = get_string("plaintext: ");
     int n = strlen(p);
-    char c[n];
-
     int i = 0;
-    while(p[i] != '\0') {
+
+    while(i < n) {
         if(isalpha(p[i])) {
-            c[i] = substituir(p[i], chave);
+            p[i] = substituir(p[i], chave);
         }
         else {
-            c[i] = p[i];
+            p[i] = p[i];
         }
         i++;
     }
 
     printf("ciphertext: ");
     i = 0;
-    while(c[i] != '\0') {
-        printf("%c", c[i]);
+    while(i < n) {
+        printf("%c", p[i]);
         i++;
     }
     printf("\n");
